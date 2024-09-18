ff8f49e87f2381d3f0f389bfdb32a224c76800ae
sophia-yb <sophiaybuda@gmail.com>
2024-09-18 09:51:06 -0700

    created drag elements

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 function showCourses() {
     let loginForm = document.getElementById("loginForm");
-    let physicsChoice = document.getElementById("physicsChoice");
-    let compsciChoice = document.getElementById("compsciChoice");
+    let physicsChoice = document.getElementById("physicsUnits");
+    let comsciChoice = document.getElementById("csUnits");
     
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault();  
@@ -9,16 +9,51 @@ function showCourses() {
         let selectedCourse = document.getElementById("courses").value;
         
         physicsChoice.style.display = "none";
-        compsciChoice.style.display = "none";
+        comsciChoice.style.display = "none";
         
         if (selectedCourse === "physics") {
             physicsChoice.style.display = "block";
-        } else if (selectedCourse === "compsci") {
-            compsciChoice.style.display = "block";
+            makeDraggable("physicsUnits");
+        } else if (selectedCourse === "comsci") {
+            comsciChoice.style.display = "block";
+            makeDraggable("csUnits");
         }
     });
 }
 
+function makeDraggable(parentId) {
+    let parent = document.getElementById(parentId);
+    let children = parent.getElementsByClassName('draggable');
+    for (let i = 0; i < children.length; i++) {
+        dragElement(children[i]);
+    }
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = function(e) {
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    };
+  
+    function elementDrag(e) {
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+}
+
 // Call the function to add the event listener to the form
 showCourses();
-
